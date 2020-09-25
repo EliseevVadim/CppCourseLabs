8f75d3147ee3483b530c6c47fb504be82c9b985e
Eliseev Vadim <55801514+EliseevVadim@users.noreply.github.com>
2020-09-25 20:29:09 +0300

    just a "little fix"

--- Lab2Cpp/Task2/Fraction.h
+++ Lab2Cpp/Task2/Fraction.h
@@ -2,7 +2,6 @@
 #include <string>
 class Fraction
 {
-
 public:	
 	static int counter;
 	Fraction();
@@ -17,12 +16,19 @@ public:
 	int gcd(int n, int m);
 	void printAsFraction(double decimal_fraction);
 	void printAsFraction(std::string decimal_fraction);
-	Fraction summarize(Fraction fraction1, Fraction fraction2);
-	Fraction subtract(Fraction fraction1, Fraction fraction2);
-	Fraction multiply(Fraction fraction1, Fraction fraction2);
-	Fraction divide(Fraction fraction1, Fraction fraction2);
 private:
 	int nominator_;
 	int denominator_;
 };
-
+inline Fraction operator + (Fraction f1, Fraction f2) {
+	return Fraction(f1.getNom() * f2.getDenom() + f2.getNom() * f1.getDenom(), f1.getDenom() * f2.getDenom());
+}
+inline Fraction operator - (Fraction f1, Fraction f2) {
+	return Fraction(f1.getNom() * f2.getDenom() - f2.getNom() * f1.getDenom(), f1.getDenom() * f2.getDenom());
+}
+inline Fraction operator * (Fraction f1, Fraction f2) {
+	return Fraction(f1.getNom() * f2.getNom(), f1.getDenom() * f2.getDenom());
+}
+inline Fraction operator / (Fraction f1, Fraction f2) {
+	return Fraction(f1.getNom() * f2.getDenom(), f1.getDenom() * f2.getNom());
+}
