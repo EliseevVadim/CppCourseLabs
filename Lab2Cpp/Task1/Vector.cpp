bde3cfef97f936503ccf976b85cecf7097785313
Eliseev Vadim <55801514+EliseevVadim@users.noreply.github.com>
2020-09-28 13:58:38 +0300

    Update Vector.cpp

--- Lab2Cpp/Task1/Vector.cpp
+++ Lab2Cpp/Task1/Vector.cpp
@@ -5,37 +5,15 @@ Vector::Vector(int length) {
 	mas = new int[length];
 	length_ = length;
 }
-void Vector::setElementToIndex(int element, int i) {
-	mas[i] = element;
-}
 void Vector::printVector() {
 	for (int i = 0; i < length_; i++) {
 		cout << mas[i] << " ";
-	}
-}
-int Vector::getElementFromIndex(int i) {
-	if (i >= length_) {
-		return NULL;
-	}
-	else
-	{
-		return mas[i];
 	}
 }
 Vector::~Vector() {
 	delete[] mas;
 	mas = nullptr;
 	cout << "\nThe vector was deleted\n";
-}
-void Vector::prefixIncrease() {
-	for (int i = 0; i < length_; i++) {
-		++mas[i];
-	}
-}
-void Vector::postfixIncrease() {
-	for (int i = 0; i < length_; i++) {
-		mas[i]++;
-	}
 }
 void Vector::prefixDecrease() {
 	for (int i = 0; i < length_; i++) {
@@ -46,4 +24,37 @@ void Vector::postfixDecrease() {
 	for (int i = 0; i < length_; i++) {
 		mas[i]--;
 	}
+}
+int& Vector::operator[](int pos) {
+	return mas[pos];
+}
+// префиксный инкремент
+Vector Vector::operator++() {
+	for (int i = 0; i < length_; i++) {
+		++mas[i];
+	}
+	return *this;
+}
+// постфиксный инкремент
+Vector Vector::operator++(int) {
+	Vector& v = *this;
+	for (int i = 0; i < length_; i++) {
+		mas[i]++;
+	}
+	return v;
+}
+// префиксный декремент
+Vector Vector::operator--() {
+	for (int i = 0; i < length_; i++) {
+		--mas[i];
+	}
+	return *this;
+}
+// постфиксный декремент
+Vector Vector::operator--(int) {
+	Vector& v = *this;
+	for (int i = 0; i < length_; i++) {
+		mas[i]--;
+	}
+	return v;
 }
