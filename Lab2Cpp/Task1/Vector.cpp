420199e8b7f0d095a6ee8ea68d384cd3aab1559a
Eliseev Vadim <55801514+EliseevVadim@users.noreply.github.com>
2020-10-05 09:13:51 +0300

    Update Vector.cpp

--- Lab2Cpp/Task1/Vector.cpp
+++ Lab2Cpp/Task1/Vector.cpp
@@ -1,13 +1,30 @@
 #include "Vector.h"
 #include <iostream>
+#include <ctime>
+#include "Matrix.h"
 using namespace std;
 Vector::Vector(int length) {
+	srand(time(NULL));
 	mas = new int[length];
 	length_ = length;
+	for (int i = 0; i < length_; i++) {
+		mas[i] = rand() % 101;
+	}
 }
 void Vector::printVector() {
 	for (int i = 0; i < length_; i++) {
 		cout << mas[i] << " ";
+	}
+}
+void Vector::getVectorFromMatrix(Matrix m) {
+	int pos = 0;
+	for (int i = 1; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (i % 2 != 0) {
+				mas[pos] = m.at(i, j);
+				pos++;
+			}
+		}
 	}
 }
 Vector::~Vector() {
@@ -15,28 +32,18 @@ Vector::~Vector() {
 	mas = nullptr;
 	cout << "\nThe vector was deleted\n";
 }
-void Vector::prefixDecrease() {
-	for (int i = 0; i < length_; i++) {
-		--mas[i];
-	}
-}
-void Vector::postfixDecrease() {
-	for (int i = 0; i < length_; i++) {
-		mas[i]--;
-	}
-}
 int& Vector::operator[](int pos) {
 	return mas[pos];
 }
 // префиксный инкремент
-Vector Vector::operator++() {
+Vector& Vector::operator++() {
 	for (int i = 0; i < length_; i++) {
 		++mas[i];
 	}
 	return *this;
 }
 // постфиксный инкремент
-Vector Vector::operator++(int) {
+Vector& Vector::operator++(int) {
 	Vector& v = *this;
 	for (int i = 0; i < length_; i++) {
 		mas[i]++;
@@ -44,14 +51,14 @@ Vector Vector::operator++(int) {
 	return v;
 }
 // префиксный декремент
-Vector Vector::operator--() {
+Vector& Vector::operator--() {
 	for (int i = 0; i < length_; i++) {
 		--mas[i];
 	}
 	return *this;
 }
 // постфиксный декремент
-Vector Vector::operator--(int) {
+Vector& Vector::operator--(int) {
 	Vector& v = *this;
 	for (int i = 0; i < length_; i++) {
 		mas[i]--;
