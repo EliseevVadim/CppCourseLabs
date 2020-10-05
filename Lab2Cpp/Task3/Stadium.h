308e98c9684562bbf869f9321fe0458a33a143ed
Eliseev Vadim <55801514+EliseevVadim@users.noreply.github.com>
2020-10-05 09:18:08 +0300

    Update Stadium.h

--- Lab2Cpp/Task3/Stadium.h
+++ Lab2Cpp/Task3/Stadium.h
@@ -19,6 +19,20 @@ public:
 	void setAverageValue(float value);
 	float getAverageValue();
 	float getPercent();
+	static void Sort(Stadium stads[3]) {
+		for (int i = 0; i < 3; i++) {
+			int max = i;
+			for (int j = i + 1; j < 3; j++) {
+				if (stads[j].getAverageValue() > stads[max].getAverageValue()) {
+					max = j;
+				}
+			}
+			std::swap(stads[max], stads[i]);
+		}
+		for (int i = 0; i < 3; i++) {
+			stads[i].printInfo();
+		}
+	}
 private:
 	std::string location_;
 	std::string club_;
@@ -27,4 +41,3 @@ private:
 	int attendance_ = 0;
 	float average;
 };
-
