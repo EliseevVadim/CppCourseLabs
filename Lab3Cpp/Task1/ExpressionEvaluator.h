cb1430abe3872d67cf50c615535d6b998970e40f
Eliseev Vadim <55801514+EliseevVadim@users.noreply.github.com>
2020-11-10 18:01:12 +0300

    ExpressionEvaluator is abstract now

--- Lab3Cpp/Task1/ExpressionEvaluator.h
+++ Lab3Cpp/Task1/ExpressionEvaluator.h
@@ -9,7 +9,7 @@ public:
 	virtual ~ExpressionEvaluator();
 	void setOperand(size_t pos, double value);
 	void setOperands(double ops[], size_t n);
-	virtual double calculate() const;
+	virtual double calculate() const = 0;
 	void logToScreen() const override;
 	void logToFile(const std::string& filename) const override;
 protected:
